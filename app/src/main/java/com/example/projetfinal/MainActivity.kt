59a076e599bd4aa5d6d1a20bac27c0e46c596b4a
Juan Camilo Beltrán Dueñas <jncamilo15@hotmail.com>
2025-03-28 15:10:07 -0400

    mi primera vez

--- app/src/main/java/com/example/projetfinal/MainActivity.kt
+++ app/src/main/java/com/example/projetfinal/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //lucas podolski
 
         englishButton = findViewById(R.id.englishButton)
         frenchButton = findViewById(R.id.frenchButton)
